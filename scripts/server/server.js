613840c774d9ac936b5bf61cd94ab7d2c511449f
CaptainJH <heqiju@gmail.com>
2014-04-26 02:00:08 +0800

    Initial submit

---
+++ scripts/server/server.js
@@ -0,0 +1,48 @@
+var 
+	http = require('http'),
+	express = require('express'),
+	connect = require('connect'),
+	app = express(),
+	server = http.createServer(app),
+	
+	mongodb = require('mongodb'),
+	mongoServer = new mongodb.Server('localhost', mongodb.Connection.DEFAULT_PORT),
+	dbHandle = new mongodb.Db('test', mongoServer, {safe : true});
+	
+dbHandle.open(function() {
+	console.log('** Connected to MongoDB **');
+});
+	
+app
+	.use(connect.logger('dev'))
+	.use(connect.errorHandler())
+	.use(connect.bodyParser())
+	.use(express.static(__dirname + '/public/html_project_ln'));
+app.get('/:doc/find/:id', function(request, response) {	
+	//response.send('Hello Express\n');
+	//response.redirect('/d3_test.html');
+	
+	dbHandle.collection(
+		request.params.doc,
+		function(outer_error, collection) {
+			console.log(request.params.id);
+			collection.find({qq:parseInt(request.params.id)}).toArray(
+				function(inner_error, map_list) {
+					response.contentType('json');
+					response.send(map_list);
+				});
+		})
+});
+app.post('/', function(request, response) {
+	response.send('POST response!\n');
+});
+app.put('/', function(request, response) {
+	response.send('PUT response!\n');
+});
+app.delete('/', function(request, response) {
+	response.send('Delete response!\n');
+});
+
+server.listen(3000);
+
+console.log( 'Express server listening on port %d in %s mode in folder %s', server.address().port, app.settings.env, __dirname + '/public' );
